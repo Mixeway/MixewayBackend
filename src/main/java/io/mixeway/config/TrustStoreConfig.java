90c76f4bb858a46dd8e08349df699ea7572589fb
siewrgrz <grzegorz.siewruk@orange.com>
2019-11-30 21:27:46 +0100

    initial CI

--- src/main/java/io/mixeway/config/TrustStoreConfig.java
+++ src/main/java/io/mixeway/config/TrustStoreConfig.java
@@ -1,7 +1,5 @@
 package io.mixeway.config;
 
-import org.omg.CORBA.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
