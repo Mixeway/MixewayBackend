5f118c89e855c96857471df099c146c4f1d21f90
Grzegorz <gsiewruk+gitlab@gmail.com>
2020-02-03 17:43:11 +0100

    debugging..

--- src/main/java/io/mixeway/plugins/infrastructurescan/nessus/apiclient/NessusApiClient.java
+++ src/main/java/io/mixeway/plugins/infrastructurescan/nessus/apiclient/NessusApiClient.java
@@ -417,7 +417,6 @@ public class NessusApiClient implements NetworkScanClient, SecurityScanner {
 
 
 	@Override
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void loadVulnerabilities(NessusScan ns) throws JSONException, CertificateException, UnrecoverableKeyException,
 			NoSuchAlgorithmException, KeyManagementException, KeyStoreException, IOException {
 		List<Interface> intfs = interfaceRepository.getInterfaceForAssetsWithHostIdSet(new ArrayList<>(ns.getProject().getAssets()));
@@ -434,7 +433,7 @@ public class NessusApiClient implements NetworkScanClient, SecurityScanner {
 		log.info("Nessus - successfully loaded vulnerabilities for {}",ns.getProject().getName());
 	}
 
-
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	private void loadVulnForInterface(NessusScan ns, Interface i) throws JSONException, CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyManagementException, KeyStoreException, IOException {
 		try {
 			i.getVulns().clear();
