11f338e16af4ba0d9d9bf0c8981a39716a6ec0f9
siewrgrz <grzegorz.siewruk@orange.com>
2020-03-16 12:53:50 +0100

    hotfix

--- src/test/java/io/mixeway/rest/admin/service/AdminUserRestServiceTest.java
+++ src/test/java/io/mixeway/rest/admin/service/AdminUserRestServiceTest.java
@@ -5,7 +5,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.omg.CORBA.UserException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
