b38ecb8136da9fbc3ea329b8b8de6975fb930e8b
Grzegorz <gsiewruk+gitlab@gmail.com>
2020-05-16 20:12:15 +0200

    normalize url fix

--- src/main/java/io/mixeway/integrations/webappscan/model/WebAppScanHelper.java
+++ src/main/java/io/mixeway/integrations/webappscan/model/WebAppScanHelper.java
@@ -7,6 +7,8 @@ public class WebAppScanHelper {
     private final static String FOUR_DIGIT_PATTERN = "[/|+|?|&|=][0-9]{4}";
     private final static String THREE_DIGIT_PATTERN = "[/|+|?|&|=][0-9]{3}";
     private final static String TWO_DIGIT_PATTERN = "[/|+|?|&|=][0-9]{2}";
+    private final static String FIVE_DIGIT_PATTERN = "[/|+|?|&|=][0-9]{5}";
+    private final static String SIX_DIGIT_PATTERN = "[/|+|?|&|=][0-9]{6}";
     private final static String TEN_DIGIT_PATTERN = "[/|+|?|&|=][0-9]{10}";
     private final static String TWELVE_DIGIT_PATTERN = "[/|+|?|&|=][0-9]{12}";
     private final static String SIM_NUMBER_PATTERN = "[/|+|?|&|=]894803[0-9]{13}";
@@ -21,7 +23,6 @@ public class WebAppScanHelper {
         //String urlToLookFor = url.replaceAll("+","\\\\+");
         String urlToLookFor=url.replaceAll(UUID_PATTERN,UUID_PATTERN);
         urlToLookFor= urlToLookFor.replace("?","\\?");
-        urlToLookFor= urlToLookFor.replace("=","\\=");
         urlToLookFor = urlToLookFor.replaceAll("\\+","\\\\+");
         urlToLookFor=urlToLookFor.replaceAll(CHAR_32_PATTERN,CHAR_32_PATTERN);
         urlToLookFor=urlToLookFor.replaceAll(AT_LEAST_ONEDIGIT,AT_LEAST_ONEDIGIT);
@@ -33,6 +34,8 @@ public class WebAppScanHelper {
         urlToLookFor=urlToLookFor.replaceAll(REFERAL_PATTERN,REFERAL_PATTERN);
         urlToLookFor=urlToLookFor.replaceAll(MSISDN_SHORT_PATTERN,MSISDN_SHORT_PATTERN);
         urlToLookFor=urlToLookFor.replaceAll(SEVEN_DIGIT_PATTERN,SEVEN_DIGIT_PATTERN);
+        urlToLookFor=urlToLookFor.replaceAll(SIX_DIGIT_PATTERN,SIX_DIGIT_PATTERN);
+        urlToLookFor=urlToLookFor.replaceAll(FIVE_DIGIT_PATTERN,FIVE_DIGIT_PATTERN);
         urlToLookFor=urlToLookFor.replaceAll(FOUR_DIGIT_PATTERN,FOUR_DIGIT_PATTERN);
         urlToLookFor=urlToLookFor.replaceAll(THREE_DIGIT_PATTERN,THREE_DIGIT_PATTERN);
         urlToLookFor=urlToLookFor.replaceAll(TWO_DIGIT_PATTERN,TWO_DIGIT_PATTERN);
