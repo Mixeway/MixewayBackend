2c3ac63c323473e7f09bbd87dc3cc50b511c730b
Grzegorz <gsiewruk+gitlab@gmail.com>
2020-06-04 11:06:28 +0200

    Removed isolation from opensourcescanservice

--- src/main/java/io/mixeway/integrations/opensourcescan/service/OpenSourceScanService.java
+++ src/main/java/io/mixeway/integrations/opensourcescan/service/OpenSourceScanService.java
@@ -98,7 +98,6 @@ public class OpenSourceScanService {
      *
      * @param codeProjectToVerify CodeProject to load opensource vulnerabilities
      */
-    @Transactional(isolation = Isolation.SERIALIZABLE)
     public void loadVulnerabilities(CodeProject codeProjectToVerify) throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyManagementException, KeyStoreException, IOException {
         for (OpenSourceScanClient openSourceScanClient : openSourceScanClients){
             if (openSourceScanClient.canProcessRequest(codeProjectToVerify)){
