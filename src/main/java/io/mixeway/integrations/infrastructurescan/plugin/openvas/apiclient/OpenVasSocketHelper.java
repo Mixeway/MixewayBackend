32f0f823831f9ac0104affc7482acc164114c941
Grzegorz <gsiewruk+gitlab@gmail.com>
2020-07-03 11:34:55 +0200

    debug openvas

--- src/main/java/io/mixeway/integrations/infrastructurescan/plugin/openvas/apiclient/OpenVasSocketHelper.java
+++ src/main/java/io/mixeway/integrations/infrastructurescan/plugin/openvas/apiclient/OpenVasSocketHelper.java
@@ -122,47 +122,12 @@ public class OpenVasSocketHelper {
      * @throws java.io.IOException
      */
     public String recv() throws java.io.IOException {
-//        int nRead;
-//        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-//        byte[] data = new byte[10240];
-//        while ((nRead = mInput.read(data, 0, data.length)) != -1) {
-//            buffer.write(data, 0, nRead);
-//        }
-//
-//        buffer.flush();
-//        byte[] byteArray = buffer.toByteArray();
-        byte[] messageByte = new byte[10000];
-        boolean end = false;
-        String dataString = "";
-        DataInputStream in = new DataInputStream(mInput);
-        int bytesRead = 0;
-
-        messageByte[0] = in.readByte();
-        messageByte[1] = in.readByte();
-        ByteBuffer byteBuffer = ByteBuffer.wrap(messageByte, 0, 2);
-
-        int bytesToRead = byteBuffer.getShort();
-        System.out.println("About to read " + bytesToRead + " octets");
-
-        //The following code shows in detail how to read from a TCP socket
-
-        while(!end)
-        {
-            bytesRead = in.read(messageByte);
-            dataString += new String(messageByte, 0, bytesRead);
-            if (dataString.length() == bytesToRead )
-            {
-                end = true;
-            }
-        }
-
-        //All the code in the loop can be replaced by these two lines
-        //in.readFully(messageByte, 0, bytesToRead);
-        //dataString = new String(messageByte, 0, bytesToRead);
-
-        System.out.println("MESSAGE: " + dataString);
-        //String text = new String(dataString, StandardCharsets.UTF_8);
-        return dataString;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(mInput));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null)
+            sb.append(line).append("\n");
+        return sb.toString();
     }
 
     /**
