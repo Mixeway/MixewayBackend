e8aad438f4c6a491652d23b2c6b709c9ae81bdf1
Grzegorz <gsiewruk+gitlab@gmail.com>
2020-07-03 12:17:53 +0200

    debug openvas

--- src/main/java/io/mixeway/integrations/infrastructurescan/plugin/openvas/apiclient/OpenVasSocketHelper.java
+++ src/main/java/io/mixeway/integrations/infrastructurescan/plugin/openvas/apiclient/OpenVasSocketHelper.java
@@ -122,12 +122,46 @@ public class OpenVasSocketHelper {
      * @throws java.io.IOException
      */
     public String recv() throws java.io.IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(mInput));
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null)
-            sb.append(line).append("\n");
-        return sb.toString();
+        byte[] messageByte = new byte[100000];
+        boolean end = false;
+        String dataString = "";
+
+        try
+        {
+            DataInputStream in = new DataInputStream(mInput);
+            int bytesRead = 0;
+
+            messageByte[0] = in.readByte();
+            messageByte[1] = in.readByte();
+            ByteBuffer byteBuffer = ByteBuffer.wrap(messageByte, 0, 2);
+
+            int bytesToRead = byteBuffer.getShort();
+            System.out.println("About to read " + bytesToRead + " octets");
+
+            //The following code shows in detail how to read from a TCP socket
+
+            while(!end)
+            {
+                bytesRead = in.read(messageByte);
+                dataString += new String(messageByte, 0, bytesRead);
+                if (dataString.length() == bytesToRead )
+                {
+                    end = true;
+                }
+            }
+
+            //All the code in the loop can be replaced by these two lines
+            //in.readFully(messageByte, 0, bytesToRead);
+            //dataString = new String(messageByte, 0, bytesToRead);
+
+            System.out.println("MESSAGE: " + dataString);
+            return dataString;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     /**
