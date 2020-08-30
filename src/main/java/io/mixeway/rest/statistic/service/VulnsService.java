57f7afa59849d00f02cd3dcdd3ff5ac4aee077b7
= <grzegorz.siewruk@orange.com>
2020-08-30 11:11:40 +0200

    permissions for statistics

--- src/main/java/io/mixeway/rest/statistic/service/VulnsService.java
+++ src/main/java/io/mixeway/rest/statistic/service/VulnsService.java
@@ -1,52 +1,93 @@
 package io.mixeway.rest.statistic.service;
 
+import io.mixeway.db.entity.Project;
 import io.mixeway.domain.service.vulnerability.VulnTemplate;
+import io.mixeway.pojo.PermissionFactory;
 import io.mixeway.pojo.VulnBarChartProjection;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class VulnsService {
     private final VulnTemplate vulnTemplate;
-    VulnsService(VulnTemplate vulnTemplate){
+    private final PermissionFactory permissionFactory;
+    VulnsService(VulnTemplate vulnTemplate, PermissionFactory permissionFactory){
         this.vulnTemplate = vulnTemplate;
+        this.permissionFactory = permissionFactory;
     }
 
-    public ResponseEntity<List<VulnBarChartProjection>> getCodeVulnsTop() {
-        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10CodeVulns(), HttpStatus.OK);
+    public ResponseEntity<List<VulnBarChartProjection>> getCodeVulnsTop(Principal principal) {
+        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository
+                .top10CodeVulns(
+                        permissionFactory
+                                .getProjectForPrincipal(principal)
+                                .stream()
+                                .map(Project::getId)
+                                .collect(Collectors.toList())),
+                HttpStatus.OK);
     }
 
-    public ResponseEntity<List<VulnBarChartProjection>> getCodeProjectsTop() {
-        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10CodeProjects(), HttpStatus.OK);
+    public ResponseEntity<List<VulnBarChartProjection>> getCodeProjectsTop(Principal principal) {
+        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10CodeProjects(permissionFactory
+                .getProjectForPrincipal(principal)
+                .stream()
+                .map(Project::getId)
+                .collect(Collectors.toList())), HttpStatus.OK);
     }
 
-    public ResponseEntity<List<VulnBarChartProjection>> getInfraVulnsTop() {
-        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10InfraVulns(), HttpStatus.OK);
+    public ResponseEntity<List<VulnBarChartProjection>> getInfraVulnsTop(Principal principal) {
+        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10InfraVulns(permissionFactory
+                .getProjectForPrincipal(principal)
+                .stream()
+                .map(Project::getId)
+                .collect(Collectors.toList())), HttpStatus.OK);
 
     }
 
-    public ResponseEntity<List<VulnBarChartProjection>> getInfraIntfsTop() {
-        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10Interfaces(), HttpStatus.OK);
+    public ResponseEntity<List<VulnBarChartProjection>> getInfraIntfsTop(Principal principal) {
+        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10Interfaces(permissionFactory
+                .getProjectForPrincipal(principal)
+                .stream()
+                .map(Project::getId)
+                .collect(Collectors.toList())), HttpStatus.OK);
 
     }
 
-    public ResponseEntity<List<VulnBarChartProjection>> getWebVulnsTop() {
-        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10WebApps(), HttpStatus.OK);
+    public ResponseEntity<List<VulnBarChartProjection>> getWebVulnsTop(Principal principal) {
+        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10WebApps(permissionFactory
+                .getProjectForPrincipal(principal)
+                .stream()
+                .map(Project::getId)
+                .collect(Collectors.toList())), HttpStatus.OK);
 
     }
 
-    public ResponseEntity<List<VulnBarChartProjection>> getWebAppsTop() {
-        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10WebAppVulns(), HttpStatus.OK);
+    public ResponseEntity<List<VulnBarChartProjection>> getWebAppsTop(Principal principal) {
+        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10WebAppVulns(permissionFactory
+                .getProjectForPrincipal(principal)
+                .stream()
+                .map(Project::getId)
+                .collect(Collectors.toList())), HttpStatus.OK);
     }
 
-    public ResponseEntity<List<VulnBarChartProjection>> getOpenSourceVulns() {
-        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10OpenSource(), HttpStatus.OK);
+    public ResponseEntity<List<VulnBarChartProjection>> getOpenSourceVulns(Principal principal) {
+        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10OpenSource(permissionFactory
+                .getProjectForPrincipal(principal)
+                .stream()
+                .map(Project::getId)
+                .collect(Collectors.toList())), HttpStatus.OK);
     }
 
-    public ResponseEntity<List<VulnBarChartProjection>> getOpenSourceVulnsForCodeProject() {
-        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10OpenSourceCodeProjects(), HttpStatus.OK);
+    public ResponseEntity<List<VulnBarChartProjection>> getOpenSourceVulnsForCodeProject(Principal principal) {
+        return new ResponseEntity<>(vulnTemplate.projectVulnerabilityRepository.top10OpenSourceCodeProjects(permissionFactory
+                .getProjectForPrincipal(principal)
+                .stream()
+                .map(Project::getId)
+                .collect(Collectors.toList())), HttpStatus.OK);
     }
 }
