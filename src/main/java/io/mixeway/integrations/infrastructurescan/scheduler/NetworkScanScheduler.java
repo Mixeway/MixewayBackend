41a7829d51ce1ef629da5079876efd29da59b141
Grzegorz Siewruk <gsiewruk+gitlab@gmail.com>
2021-03-23 07:04:43 +0100

    Removing time restriction on network vuln load

--- src/main/java/io/mixeway/integrations/infrastructurescan/scheduler/NetworkScanScheduler.java
+++ src/main/java/io/mixeway/integrations/infrastructurescan/scheduler/NetworkScanScheduler.java
@@ -23,21 +23,7 @@ public class NetworkScanScheduler {
 
 	@Scheduled(initialDelay=0,fixedDelay = 60000)
 	public void checkScanStatus(){
-		ExecutorService executor = Executors.newSingleThreadExecutor();
-		Future<String> future = executor.submit((Callable) () -> {
-			networkScanService.scheduledCheckStatusAndLoadVulns();
-			return "OK";
-		});
-		try {
-			future.get(3, TimeUnit.MINUTES);
-		} catch (TimeoutException ex) {
-			log.warn("[NetworkScanServiceScheduler] 3 minutes lmit exceeded for loading vulns... interrupting..");
-		} catch (InterruptedException e) {
-			log.warn("[NetworkScanServiceScheduler] Vuln loading interrupted");
-		} catch (ExecutionException e) {
-			log.warn("[NetworkScanServiceScheduler] ExecutionException during vuln loading..");
-		}
-		executor.shutdownNow();
+		networkScanService.scheduledCheckStatusAndLoadVulns();
 
 	}
 	@Scheduled(cron="#{@getNetworkCronExpresion}" )
