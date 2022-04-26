e79e67410f8695e9c2df703edba9f5cd0401ace0
= <grzegorz.siewruk@orange.com>
2022-04-26 12:59:32 +0200

    lombok

--- src/main/java/io/mixeway/scanmanager/model/AssetToCreate.java
+++ src/main/java/io/mixeway/scanmanager/model/AssetToCreate.java
@@ -1,14 +1,12 @@
 package io.mixeway.scanmanager.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class AssetToCreate {
 	private String hostname;
 	private String ip;
