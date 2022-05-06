68623ce8c35badeb7562c971188c4b1a1ea0f27f
Grzegorz Siewruk <grzegorz.siewruk@orange.com>
2022-05-06 12:53:53 +0200

    resolve bug with inproper auth/status returned values

--- src/main/java/io/mixeway/api/auth/model/StatusEntity.java
+++ src/main/java/io/mixeway/api/auth/model/StatusEntity.java
@@ -1,12 +1,14 @@
 package io.mixeway.api.auth.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class StatusEntity {
     Boolean initialized;
     Boolean password;
